02c85ea5d906abde5e8d3beeffe13ae76e923005
H <161506051+Hahaverse@users.noreply.github.com>
2024-03-02 18:13:12 +0900

    first commit
    도형을 설정하는 프로그램입니다.

---
+++ 소스.cpp
@@ -0,0 +1,56 @@
+//2023111359 최가윤
+#include <iostream>
+using namespace std;
+
+class Shape {
+private:
+    double height;
+    double width;
+public:
+    Shape(double h, double w) {
+        height = h;
+        width = w;
+    }
+    void setHeight(double h) {
+        height = h;
+    }
+    void setWidth(double w) {
+        width = w;
+    }
+    double getHeight() {
+        return height;
+    }
+    double getWidth() {
+        return width;
+    }
+};
+
+class Rectangle : public Shape {
+private:
+public:
+    Rectangle() :Shape(0.0, 0.0) {}
+    Rectangle(double h, double w) :Shape(h, w) {}
+    double getArea() {
+        return getHeight() * getWidth();
+    }
+};
+
+class Triangle : public Shape {
+private:
+public:
+    Triangle() :Shape(0.0, 0.0) {}
+    Triangle(double h, double w) :Shape(h, w) {}
+    double getArea() {
+        return getHeight() * getWidth() / 2;
+    }
+};
+
+int main() {
+    Rectangle r(5, 2);
+    Triangle t(3, 5);
+
+    cout << "Rectangle(5,2)의 면적 : " << r.getArea() << endl;
+    cout << " Triangle(3,5)의 면적 : " << t.getArea() << endl;
+
+    return 0;
+}
